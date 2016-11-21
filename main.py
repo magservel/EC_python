d95e6b35d0b04ddf522c28a555d09a7b20e96636
magservel <mag.servel@gmail.com>
2016-11-22 00:04:33 +0100

    Diffre-Hellman Implemented Next step:  Implement El Gammal

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from DH import *
 
 path = "/home/magali/PycharmProjects/EC/elliptic_curves/Weierstrass/"
 folder = "cw256/"
-file = "w256-002.gp"
+file = "w256-001.gp"
 file_path = path + folder + file
 print file_path
 
